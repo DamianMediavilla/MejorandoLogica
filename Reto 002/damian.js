d37c60c9bf9cea0487aba4abadf6686a5b5915f8
Damian Mediavilla <damianmediavilla@hotmail.com>
2023-02-18 11:27:11 +0100

    first commit

---
+++ Reto 002/damian.js
@@ -0,0 +1,39 @@
+morseCode = '.... . -.--   .--- ..- -.. .'
+decodeMorse = function(morseCode){
+    // Your code here
+    // You can use MORSE_CODE[morse]
+    const mensajeMorseArray = []
+    mensajeMorse = morseCode.trim().split("   ");
+    mensajeMorse.forEach((palabraMorse)=>{
+      palabraMorseArray = palabraMorse.split(" ")
+      mensajeMorseArray.push(palabraMorseArray)
+    
+    })
+  
+    
+    let mensaje =[]
+    mensajeMorseArray.forEach((palabraMorseArray)=>{
+      let palabra =[]
+      palabraMorseArray.forEach((letraMorse)=>{
+        
+        letra = MORSE_CODE[letraMorse]
+        palabra.push(letra)
+      })
+      mensaje.push(palabra)
+    })
+    
+    let mensajeS = ' '
+    mensaje.forEach((palabra)=>{
+      palabraS =''
+       palabra.forEach((letra)=>{
+         palabraS = palabraS + letra
+         })
+      mensajeS = mensajeS + palabraS + ' '
+      
+    })
+    console.log(`${mensajeS.trim().toString()}`)
+    return mensajeS.trim().toString()
+    
+  }
+  
+  
